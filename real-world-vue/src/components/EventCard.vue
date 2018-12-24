4c71e5f04a83d714153df25e346cc3bc04e623bc
Cristian Asenjo <cristian.a.asenjo@gmail.com>
2018-12-23 22:27:50 -0500

    Real World Vue lesson 3

---
+++ real-world-vue/src/components/EventCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1 v-if @click="handler(arg, event)"></h1>
+</template>
+
+<script>
+import { libName } from 'libName'
+export default {
+  data() {
+    return {
+      quote: 'I like VueJS'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
